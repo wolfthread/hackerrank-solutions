6a80462e325f9940769ca24f3f3d97e62810e064
Sylvain Dessureault <wolfthread2021@gmail.com>
2020-12-30 11:25:12 -0500

    Add day 2 of 30DofC cpp and php solutions and add an algo implementation solution

---
+++ algorithms/implementation/easy/repeated-string.py
@@ -0,0 +1,9 @@
+def repeatedString(s, n):
+    l_of_string = len(s)
+    remainder = n % l_of_string
+    if  remainder == 0:
+        return s.count("a") * (n//l_of_string)
+    else:
+        base_count = s.count("a") * (n//l_of_string)
+        additional_count = s[:remainder].count("a")
+        return base_count + additional_count
