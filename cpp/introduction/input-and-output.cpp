5d74e5526215fbbcb4bf492a0456b6fd0e0ab142
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-10 16:58:02 -0500

    Add cpp solution

---
+++ cpp/introduction/input-and-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int total = 0;
+    int a, b, c;
+    cin >> a >> b >> c;
+    total += a + b + c;
+    cout << total << endl;
+    return 0;
+}
