0a13402e147c0f97d28287b8fc27d8b5f8fb86b2
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-12 09:21:07 -0500

    Fix typo

--- 30-days-of-code/cpp/day-13-abstract-classes.cpp
+++ 30-days-of-code/cpp/day-13-abstract-classes.cpp
@@ -44,12 +44,12 @@ class MyBook: public Book {
 };
 
 int main() {
-    string title,author;
+    string title, author;
     int price;
-    getline(cin,title);
-    getline(cin,author);
-    cin>>price;
-    MyBook novel(title,author,price);
+    getline(cin, title);
+    getline(cin, author);
+    cin >> price;
+    MyBook novel(title,author, price);
     novel.display();
     return 0;
 }
