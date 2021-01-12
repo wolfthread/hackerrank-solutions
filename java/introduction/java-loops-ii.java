c06c6ddc4e1dd881c14b8a5e3339949a2463a0dd
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-11 20:26:08 -0500

    Add a couple of java solutions

---
+++ java/introduction/java-loops-ii.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+// We use a, b and n to create the following series:
+// (a + 2^0*b), (a + 2^0*b + 2^1*b),...,(a + 2^0*b + 2^1 + ... + 2^n-1*b)
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int currCalc = a;
+            for(int j = 0; j < n; j++) {
+                currCalc += Math.pow(2,j) * b;
+                System.out.print(String.format("%d ", currCalc));
+            };
+            System.out.println("");
+        };
+        in.close();
+    }
+}
