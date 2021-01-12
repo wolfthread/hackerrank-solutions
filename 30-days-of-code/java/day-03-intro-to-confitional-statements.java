265a74b21f6b5e8b80bb1021fbaa98ecabd27e93
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-12 09:58:55 -0500

    Add a few 30DaysOfCode java solutions

---
+++ 30-days-of-code/java/day-03-intro-to-confitional-statements.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        String w = "Weird";
+        String nw = "Not Weird";
+        if (N % 2 != 0) {
+            System.out.println(w);
+        } else if (N >= 2 && N <= 5) {
+            System.out.println(nw);
+        } else if (N >= 6 && N <= 20) {
+            System.out.println(w);
+        } else {
+            System.out.println(nw);
+        }
+
+        scanner.close();
+    }
+}
