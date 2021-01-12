b52aebd26b5d8f9fd66e32ae214b6d034ea88907
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-12 10:43:01 -0500

    Add a couple of java solutions in 30DaysOfCode

---
+++ 30-days-of-code/java/day-10-binary-numbers.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int current_count = 0;
+        int max_count = 0;
+        for(int i=0; n>0; i++) {    
+            if(n%2 == 1){
+                current_count++;
+            } else {
+                current_count = 0;
+            }
+            if(current_count > max_count){
+                max_count = current_count;
+            };
+            n = n/2;
+        }
+        System.out.println(max_count);
+        
+        scanner.close();
+    }
+}
