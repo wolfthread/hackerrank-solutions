134f03ee5d46320ddec4dab8004a2de5da5122be
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-12 10:15:52 -0500

    Add a couple of java solutions for 30DaysOfCode

---
+++ 30-days-of-code/java/day-06-lets-review.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        int nb_tests;
+        Scanner scan = new Scanner(System.in);
+        nb_tests = scan.nextInt();
+        scan.nextLine();
+        for (int i=0; i<nb_tests; i++) {
+            String my_string;
+            String left_side = "";
+            String right_side = "";
+            my_string = scan.nextLine();
+            char[] myCharArray = my_string.toCharArray();
+            for (int j=0; j<my_string.length();j++){
+                if (j % 2 == 0) {
+                    left_side += myCharArray[j];
+                } else {
+                    right_side += myCharArray[j];
+                };
+            };
+            System.out.println(String.format("%s%s%s", left_side, " ", right_side));
+        }
+    }
+}
