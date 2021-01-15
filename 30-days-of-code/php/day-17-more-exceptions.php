8b41899bd59550572512e69d1319b756057d1237
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-15 11:18:51 -0500

    Add Day 17 of 30DaysOfCode php solution

---
+++ 30-days-of-code/php/day-17-more-exceptions.php
@@ -0,0 +1,38 @@
+<?php
+
+class NegativeException extends Exception {
+    
+    public function __construct($message, $code=0) {
+        parent::__construct($message,$code);
+    }   
+
+    public function __toString() {
+        return $this->message;
+    }
+    
+} 
+
+class Calculator {
+    public function power($a, $b) {
+        if ($a < 0 || $b < 0) {
+            throw new NegativeException("n and p should be non-negative");
+        } else {
+            return pow($a, $b);
+        }
+    }
+}
+   
+
+$myCalculator=new Calculator();
+$T=intval(fgets(STDIN));
+while($T-->0){
+    list($n, $p)  = explode(" ", trim(fgets(STDIN)));
+    try{
+        $ans=$myCalculator->power($n,$p);
+        echo $ans."\n";
+    }
+    catch(Exception $e){
+        echo $e->getMessage()."\n";
+    }
+}
+?>
