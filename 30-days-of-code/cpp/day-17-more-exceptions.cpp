f085f460814c3192993b95956ec489d6604c8a3c
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-01-15 10:53:57 -0500

    Add Day 17 of 30DaysOfCode cpp solution

---
+++ 30-days-of-code/cpp/day-17-more-exceptions.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+using namespace std;
+
+struct NegativeException : public exception {
+    NegativeException () {}
+    const char * what() const throw() {
+        return "n and p should be non-negative";
+    }
+};
+
+class Calculator {
+    public:
+        int power(int a, int b) {
+            if (a < 0 || b < 0) {
+                throw NegativeException();
+            } else {
+                return pow(a, b);
+            }
+        }
+};
+
+int main()
+{
+    Calculator myCalculator=Calculator();
+    int T,n,p;
+    cin>>T;
+    while(T-->0){
+      if(scanf("%d %d",&n,&p)==2){
+         try{
+               int ans=myCalculator.power(n,p);
+               cout<<ans<<endl; 
+         }
+         catch(exception& e){
+             cout<<e.what()<<endl;
+         }
+      }
+    }
+    
+}
