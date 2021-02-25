e4d47ec84573c65e1cfd16fdee217fcbb31f5987
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-02-25 06:46:28 -0500

    Add circular array

---
+++ algorithms/implementation/easy/circular-array-rotation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Begin Solution
+    //---------------------------------------------------------------------------------------
+    long int n, k, q;
+    cin >> n >> k >> q;
+    long arr[n];
+    long queries[q];
+    long rotated[n];
+    for (int i = 0; i < n ; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < q; i++) {
+        cin >> queries[i];
+    }
+    for (int i = 0; i < n; i ++) {
+        if (i < k) {
+            rotated[i] = arr[n - k + i];
+        } else {
+            rotated[i] = arr[i - k];
+        }
+    }
+    for (int i = 0 ; i < q; i ++) {
+        cout << rotated[queries[i]] << endl;
+    }
+    //---------------------------------------------------------------------------------------
+    // End Solution
+}
