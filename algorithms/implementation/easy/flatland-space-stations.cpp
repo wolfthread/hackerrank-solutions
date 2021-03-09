4d31336234bc441d6350f15123aeb3ba9a1abe96
Sylvain Dessureault <wolfthread2021@gmail.com>
2021-03-09 15:23:59 -0500

    Add flatland space stations cpp solution in algo track

---
+++ algorithms/implementation/easy/flatland-space-stations.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+// Begin Solution
+//---------------------------------------------------------------------------------------
+    int n, m;
+    cin >> n >> m;
+    if (n == m) {
+        cout << 0 << endl;
+    }
+    else {
+
+        int stations[m];
+        for (int i = 0; i < m; i++) {
+            cin >> stations[i];
+        }
+        sort(stations, stations + m);
+        int maxDistance = stations[0];
+        for (int i = 0; i < m - 1; i++) {
+            int distance = (stations[i + 1] - stations[i]) / 2;
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        if (n - stations[m - 1] > maxDistance) {
+            maxDistance = n - stations[m - 1] - 1;
+        }
+        cout << maxDistance << endl;
+    }
+    return 0;
+//---------------------------------------------------------------------------------------
+// End Solution
+}
